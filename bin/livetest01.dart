5359c9144b67d8f5859b86c309959f6686cd13c6
MehediAsif2 <120641634+MehediAsif2@users.noreply.github.com>
2024-03-16 00:01:56 +0600

    Initial commit

---
+++ bin/livetest01.dart
@@ -0,0 +1,21 @@
+void main(){
+  var fruitList= <Map>
+
+  [{ "name": "Apple",
+      "color": "Red",
+      "Price": "25 usd"
+    },
+    { "name": "Banana",
+      "color": "Yellow",
+      "Price": "1.0 usd"
+    },
+    {
+      "name": "Grapes",
+      "color": "Purple",
+      "Price": "3.0 usd"
+    }
+  ];
+  print(fruitList);
+}
+//After10%discount
+
